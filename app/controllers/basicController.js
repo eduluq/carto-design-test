eb483a495264dbc9493e185e94f364f70fbfa9ce
Eduardo Luque <edu@eduluque.com>
2017-11-22 13:40:34 +0100

    Markdown support added

--- app/controllers/basicController.js
+++ app/controllers/basicController.js
@@ -1,3 +1,8 @@
+//Dependencies
+var fs = require("fs");
+var path = require("path")
+var marked = require('marked');
+
 var basicController = {};
 
 basicController.get = (req, res) => {
@@ -9,8 +14,36 @@ basicController.getDocs = (req, res) => {
 }
 
 basicController.getArticle = (req, res) => {
-  console.log(req.params.article)
-  res.render("pages/article");
+
+  //Menu
+  var menu = [{title: "Home", files: []}, {title: "Client Libraries", files: ["CARTO.js", "Getting Started", "General Concepts", "Named Maps", "Specific UI Functions", "Events", "API Reference"]},{title: "Location Data Services", files: ["Routing"]},{title: "Builder",files: ["Static", "Tiles", "WMS"]}];
+
+  //Get markdown from file
+  var file = req.params.article.slice(1);
+  var filePath = path.join(__dirname, "../../data/") + file + ".txt";
+  fs.readFile(filePath, "utf8", (err, data) => {
+
+    if(err) {
+      console.log("error reading file");
+    }
+
+    else {
+
+      var sections = data.split('\n\n\n');
+      var html = "";
+      for (var i = 0; i < sections.length; i++) {
+        var aux = "<section>" + marked(sections[i]) + "</section>"
+        html += aux
+      }
+
+      res.render("pages/article", {
+        menu: menu,
+        content: html
+      });
+    }
+
+  });
+
 }
 
 exports.basicController = basicController;
